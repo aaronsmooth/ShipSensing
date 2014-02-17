4b851c4c2b6b0ae9cd3ee5a1bf98bcd38051913b
root <root@raspberrypi.(none)>
2014-02-16 21:16:41 -0800

    Finished calibrating the sensor and formatting for the LCD

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <wiringPi.h>
 #include <lcd.h>
+#include <math.h>
 
 int convertOutput(int msb, int lsb);
 
@@ -15,18 +16,46 @@ int main(int argc, char *argv[]) {
 	lcdHome(lcd);
 	int nothing;
 	nothing = wiringPiI2CWriteReg8(fd, 0x02, 0x00);
+	wiringPiI2CWriteReg8(fd, 0x00, 0x70);
+	wiringPiI2CWriteReg8(fd, 0x01, 0xE0);
 	if (!nothing) {
 		for(;;) {
 			while(!(wiringPiI2CReadReg8(fd, 0x09) & 0x01));
-			//int output = wiringPiI2CReadReg8(fd, 0x3D);
-			int xOutput = convertOutput(wiringPiI2CReadReg8(fd, 0x03), wiringPiI2CReadReg8(fd, 0x04));
-			int yOutput = convertOutput(wiringPiI2CReadReg8(fd, 0x07), wiringPiI2CReadReg8(fd, 0x08));
-			int zOutput = convertOutput(wiringPiI2CReadReg8(fd, 0x05), wiringPiI2CReadReg8(fd, 0x06));
-			//printf("\ec");
-			//printf("xOutput is: %d \n", xOutput);
-			//printf("yOutput is: %d \n", yOutput);
-			//printf("zOutput is: %d \n", zOutput);
-			lcdPrintf(lcd,"x:%5d y:%5d z:%5d         ",xOutput,yOutput,zOutput);
+			float xOutput = convertOutput(wiringPiI2CReadReg8(fd, 0x03), wiringPiI2CReadReg8(fd, 0x04))/(float)230;
+			float yOutput = convertOutput(wiringPiI2CReadReg8(fd, 0x07), wiringPiI2CReadReg8(fd, 0x08))/(float)230;
+			float zOutput = convertOutput(wiringPiI2CReadReg8(fd, 0x05), wiringPiI2CReadReg8(fd, 0x06))/(float)230;;
+			printf("\ec");
+			printf("xOutput is: %6.2f \n", xOutput);
+			printf("yOutput is: %6.2f \n", yOutput);
+			printf("zOutput is: %6.2f \n", zOutput);
+			float magnitude = sqrt(pow(xOutput,2)+pow(yOutput,2)+pow(zOutput,2));
+			printf("Gauss Magnitude is: %8.6f\n", magnitude);
+			float heading = atan2(yOutput, xOutput);
+			if(heading < 0){
+				heading += 2*M_PI;
+			}else if(heading > 2*M_PI){
+				heading -= 2*M_PI;
+			}
+			heading = heading * (float)180/M_PI;
+			printf("Heading in degrees is: %4.1f\n", heading);
+			float pitch = atan2(sqrt(pow(xOutput,2)+pow(yOutput,2)), zOutput);
+			if(pitch < 0){
+			pitch += 2*M_PI;
+			}else if(pitch > 2*M_PI){
+			pitch -= 2*M_PI;
+			}
+			pitch = pitch * (float)180/M_PI;
+			printf("Pitch in degrees is: %4.1f\n", pitch);
+			// LCD screen is formatted as following
+			// x-gauss  y-gauss  z-gauss
+			// vector-gauss-magnitude  x-y-heading-in-degrees  z-pitch-in-degrees
+			// Example:
+			// 1234567890123456 16x2 character LCD
+			// -1.23-1.23 -1.23
+			// 1.234567 360 360
+			lcdClear(lcd);
+			lcdHome(lcd);
+			lcdPrintf(lcd,"% 1.2f% 1.2f % 1.2f%8.6f %3.0f %3.0f",xOutput,yOutput,zOutput,magnitude,heading,pitch);
 		}
 	}
 	return 0;
