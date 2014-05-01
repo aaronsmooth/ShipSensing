08e18233b2f43410703da86a2824630566ce2ea1
Aaron Smooth <aaronsmooth@Hotmail.com>
2014-05-01 03:45:02 +0000

    moved files around

---
+++ pi-c/SeniorProjectCver/3.simple_data/main.c
@@ -0,0 +1,127 @@
+/*
+	libxbee - a C library to aid the use of Digi's XBee wireless modules
+	          running in API mode.
+
+	Copyright (C) 2009 onwards  Attie Grande (attie@attie.co.uk)
+
+	libxbee is free software: you can redistribute it and/or modify it
+	under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	libxbee is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with libxbee. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <xbee.h>
+
+char message[8] = "00000000";;
+
+void myCB(struct xbee *xbee, struct xbee_con *con, struct xbee_pkt **pkt, void **data) {
+	if ((*pkt)->dataLen > 0) {
+		if ((*pkt)->data[0] == '@') {
+			xbee_conCallbackSet(con, NULL, NULL);
+			printf("*** DISABLED CALLBACK... ***\n");
+		}
+		printf("rx: [%s]\n", (*pkt)->data);
+	}
+	printf("tx: %d\n", xbee_conTx(con, NULL, "Hello\r\n"));
+}
+
+int main(void) {
+        time_t rawtime;
+	struct tm * timeinfo;
+	void *d;
+	struct xbee *xbee;
+	struct xbee_con *con;
+	struct xbee_conAddress address;
+	xbee_err ret;
+
+	if ((ret = xbee_setup(&xbee, "xbeeZB", "/dev/ttyAMA0", 9600)) != XBEE_ENONE) {
+		printf("ret: %d (%s)\n", ret, xbee_errorToStr(ret));
+		return ret;
+	}
+
+	memset(&address, 0, sizeof(address));
+	address.addr64_enabled = 1;
+	address.addr64[0] = 0x00;
+	address.addr64[1] = 0x13;
+	address.addr64[2] = 0xA2;
+	address.addr64[3] = 0x00;
+	address.addr64[4] = 0x40;
+	address.addr64[5] = 0x8B;
+	address.addr64[6] = 0xD0;
+	address.addr64[7] = 0xF8;
+	if ((ret = xbee_conNew(xbee, &con, "Data", &address)) != XBEE_ENONE) {
+		xbee_log(xbee, -1, "xbee_conNew() returned: %d (%s)", ret, xbee_errorToStr(ret));
+		return ret;
+	}
+
+	if ((ret = xbee_conDataSet(con, xbee, NULL)) != XBEE_ENONE) {
+		xbee_log(xbee, -1, "xbee_conDataSet() returned: %d", ret);
+		return ret;
+	}
+
+	if ((ret = xbee_conCallbackSet(con, myCB, NULL)) != XBEE_ENONE) {
+		xbee_log(xbee, -1, "xbee_conCallbackSet() returned: %d", ret);
+		return ret;
+	}
+	
+	/* get current time */
+	time(&rawtime);
+	/* get local time from current time */
+	timeinfo = localtime (&rawtime);
+
+	int i;
+	/* kick off the chain reaction! */
+	for (;;) {
+		void *p;
+		time(&rawtime);
+		
+		timeinfo = localtime (&rawtime);
+		//printf("time %d", timeinfo->tm_sec );
+
+		message[0] = 'H' + '0';
+		message[1] = timeinfo->tm_sec + '0'; //0 seconds
+		message[2] = timeinfo->tm_min + '0'; //31 minutes
+		message[3] = timeinfo->tm_hour + '0'; //20th hour or 8pm
+		message[4] = timeinfo->tm_wday + '0'; //
+		message[5] = timeinfo->tm_mday + '0';
+		message[6] = timeinfo->tm_mon + '0';
+		message[7] = timeinfo->tm_year + '0';
+		
+		xbee_conTx(con, NULL, message);
+		for (i = 0; i < 8;i ++) {
+			printf("%d ", message[i] - '0');
+		}
+		printf("\n");
+
+		if ((ret = xbee_conCallbackGet(con, (xbee_t_conCallback*)&p)) != XBEE_ENONE) {
+			xbee_log(xbee, -1, "xbee_conCallbackGet() returned: %d", ret);
+			return ret;
+		}
+
+		//printf("\n%d\n", ret);
+		if (p == NULL) break;
+
+		usleep(1000000);
+	}
+	
+	if ((ret = xbee_conEnd(con)) != XBEE_ENONE) {
+		xbee_log(xbee, -1, "xbee_conEnd() returned: %d", ret);
+		return ret;
+	}
+
+	xbee_shutdown(xbee);
+
+	return 0;
+}
