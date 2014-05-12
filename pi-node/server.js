47319663e342209507334f72d7a8173d31fa1770
pasang sherpa <pasang_sh@hotmail.com>
2014-05-11 23:19:48 -0700

    node server in progress

--- pi-node/server.js
+++ pi-node/server.js
@@ -12,39 +12,91 @@ var express = require('express'),
     Parse = require('parse').Parse,
     RaspiCam = require('raspicam'),
     camera = new RaspiCam({
-        mode: "timelapse",
+        mode: "photo",
         output: "./photo/img.jpg",
         encoding: "jpg",
-        timelapse: 200,
-        timeout: 86400000
+        timeout: 0
     }),
     port = process.env.PORT || 8000,
     app = express();
 
 Parse.initialize("YwcaugA0e48pvA2Rsmj7yIT9GbOHCPitW3LDPnlq", "KI3qObjuxgfRLTosry7qgmOOFErXEYfuzxgJkiEg");
-camera.start();
 
-function recordActivity(path, value, callback) {
+app.configure(function() {
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+});
+
+app.get("/save/:mmsi/:utime", function(req, res) {
+    var mmsi = req.params.mmsi,
+        utime = req.params.utime;
+
+    console.log("mmsi:" + mmsi)
+    console.log("utime:" + utime)
     var Activity = Parse.Object.extend("Activity"),
         activity = new Activity(),
-        image, fileData, now = new Date();
-    filedata = Array.prototype.slice.call(new Buffer(fs.readFileSync('/home/pi/projects/rsas-pi/pi/photo/img.jpg')), 0)
-    image = new Parse.File("image.jpg", filedata);
+        image, fileData, dockedAt = new Date(utime * 1000);
+    filedata = Array.prototype.slice.call(new Buffer(fs.readFileSync("/home/pi/projects/ShipSensing/pi-node/photo/img.jpg")), 0)
+    image = new Parse.File("img.jpg", filedata);
     image.save().then(function(file) {
         console.log("saving activity to parse");
-        activity.set("enteredAt", now);
-        activity.set("value", value);
-        activity.set("photo", file);
-        activity.set("photoUrl", file.url());
+        activity.set("vesselMmsi", mmsi);
+        activity.set("dockedAt", dockedAt);
+        activity.set("image", file);
+        activity.save();
+        return res.send(activity);
+    });
+});
+
+app.get("/add/:mmsi/:utime", function(req, res) {
+    console.log("add activity requested");
+    var mmsi = req.params.mmsi,
+        utime = req.params.utime;
+    if (!mmsi || !utime) {
+        return res.send(400, "invalid inputs");
+    }
+    camera.start();
+    camera.on("read", function(err, timestamp, filename) {
+        camera.stop();
+        console.log("photo image captured with filename: " + filename);
+        return res.redirect('save/' + mmsi + "/" + utime);
+    });
+});
+
+function recordActivity(mmsi, utime, callback) {
+    var Activity = Parse.Object.extend("Activity"),
+        activity = new Activity(),
+        image, fileData, dockedAt = new Date(utime * 1000);
+    filedata = Array.prototype.slice.call(new Buffer(fs.readFileSync("/home/pi/projects/ShipSensing/pi-node/photo/img.jpg")), 0)
+    image = new Parse.File("img.jpg", filedata);
+    image.save().then(function(file) {
+        console.log("saving activity to parse");
+        activity.set("vesselMmsi", mmsi);
+        activity.set("dockedAt", dockedAt);
+        activity.set("image", file);
         activity.save();
         callback(activity);
     });
 }
 
-app.post("/activity/add", function(req, res) {
-    /* some server side logic */
-    console.log("add activity")
-    res.send("OK");
+app.post("/activity", function(req, res) {
+    console.log("post activity requested");
+    var mmsi = req.body.mmsi,
+        utime = req.body.utime;
+
+    console.log("mmsi:" + mmsi)
+    console.log("utime:" + utime)
+    if (!mmsi || !utime) {
+        return res.send(400, "invalid inputs");
+    }
+    camera.start();
+    camera.on("read", function(err, timestamp, filename) {
+        camera.stop();
+        console.log("photo image captured with filename: " + filename);
+        recordActivity(mmsi, utime, function(activity) {
+            return res.send(activity);
+        });
+    });
 });
 
 app.get("*", function(req, res) {
