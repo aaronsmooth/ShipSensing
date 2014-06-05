758ce018c359b34354a8fe17c16e1291c25c53cb
pasang sherpa <pasang_sh@hotmail.com>
2014-06-04 21:44:46 -0700

    message fix with emotions

--- web/cloud/main.js
+++ web/cloud/main.js
@@ -8,11 +8,16 @@
 require('cloud/app');
 
 Parse.Cloud.afterSave("Activity", function(request, response) {
-    var query = new Parse.Query(Parse.Installation);
+    var query = new Parse.Query(Parse.Installation),
+        activity = request.object,
+        status = activity.get("status"),
+        smiley = status == "Arrived" ? " :)" : " :(",
+        message = "There is an activity in EB1. [ Vessel " + status.toLowerCase() + smiley + " ]";
+
     Parse.Push.send({
         where: query,
         data: {
-            alert: "There is some activity at EB1.",
+            alert: message,
             title: "Dock Monitor"
         }
     }, {
